fbadf16c9fbd28286746adac0dfdaf3c77b9f258
Pooja D <poojadeshmukhcsn@gmail.com>
2026-09-18 15:16:48 +0000

    commit

---
+++ models/incr_changes.sql
@@ -0,0 +1,14 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='empno',
+        on_schema_change='append_new_columns'
+    )
+}}
+
+select * from dev_db.bronze.incr_changes
+
+{% if is_incremental() %}
+    -- this filter will only be applied on an incremental run
+    where d_upd_Date > (select max(d_upd_Date) from {{ this }}) 
+{% endif %}
